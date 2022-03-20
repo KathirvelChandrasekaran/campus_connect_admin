09c72cc151148e8bc6e62546f8e37d22103877d3
KathirvelChandrasekaran <kathirvel98@outlook.com>
2022-03-20 12:14:22 +0530

    feat: searching logic and display result

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -13,7 +13,30 @@ class SearchScreen extends StatefulWidget {
 }
 
 class _SearchScreenState extends State<SearchScreen> {
-  TextEditingController _searchText = TextEditingController();
+  final TextEditingController _searchText = TextEditingController();
+  List<dynamic>? students;
+  bool isLoading = false;
+  bool isDataLoaded = false;
+
+  getSearchResults() async {
+    setState(() {
+      isDataLoaded = false;
+      isLoading = true;
+    });
+
+    var result = await supabase
+        .from('students')
+        .select()
+        .like('email_id', '%${_searchText.text}%')
+        .execute();
+
+    setState(() {
+      students = result.data;
+      isDataLoaded = true;
+      isLoading = false;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Consumer(
@@ -29,16 +52,16 @@ class _SearchScreenState extends State<SearchScreen> {
             body: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Container(
-                  padding: const EdgeInsets.all(16),
+                Padding(
+                  padding: const EdgeInsets.only(left: 16.0, right: 16.0),
                   child: Text(
-                    'Search',
+                    'Search Students',
                     style: Theme.of(context).textTheme.headline4,
                   ),
                 ),
                 const SizedBox(height: 16),
                 Padding(
-                  padding: const EdgeInsets.all(10.0),
+                  padding: const EdgeInsets.all(16.0),
                   child: TextFormField(
                     controller: _searchText,
                     decoration: InputDecoration(
@@ -67,6 +90,7 @@ class _SearchScreenState extends State<SearchScreen> {
                       suffixIcon: IconButton(
                         onPressed: () {
                           _searchText.clear();
+                          isDataLoaded = false;
                         },
                         icon: const Icon(
                           FluentIcons.text_clear_formatting_24_filled,
@@ -74,24 +98,82 @@ class _SearchScreenState extends State<SearchScreen> {
                         color: Theme.of(context).colorScheme.error,
                       ),
                     ),
-                    style: const TextStyle(color: Colors.white),
+                    style: TextStyle(
+                      color: theme.darkTheme
+                          ? Theme.of(context).primaryColor
+                          : Colors.black,
+                    ),
                   ),
                 ),
                 const SizedBox(height: 10),
                 Center(
                   child: RoundedButtonWidget(
-                    buttonText: "Search",
-                    width: MediaQuery.of(context).size.width * 0.95,
-                    onpressed: () async {
-                      var result = await supabase
-                          .from('students')
-                          .select()
-                          .textSearch('roll_number', _searchText.text)
-                          .execute();
-                      print(result.data);
-                    },
+                    buttonText: "Search Students",
+                    width: MediaQuery.of(context).size.width * 0.90,
+                    onpressed: getSearchResults,
                   ),
                 ),
+                const SizedBox(height: 10),
+                isDataLoaded
+                    ? Expanded(
+                        child: Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: ListView(
+                            children: students!
+                                .map(
+                                  (student) => Column(
+                                    children: [
+                                      Container(
+                                        decoration: BoxDecoration(
+                                          borderRadius:
+                                              BorderRadius.circular(15),
+                                          color: Theme.of(context).primaryColor,
+                                        ),
+                                        child: ListTile(
+                                          title: Text(
+                                            student['roll_number'],
+                                            style: const TextStyle(
+                                              color: Colors.black,
+                                            ),
+                                          ),
+                                          subtitle: Text(
+                                            student['email_id'],
+                                            style: const TextStyle(
+                                              color: Colors.black,
+                                            ),
+                                          ),
+                                          trailing: Text(
+                                            student['department'],
+                                            style: const TextStyle(
+                                              color: Colors.black,
+                                              fontWeight: FontWeight.bold,
+                                            ),
+                                          ),
+                                        ),
+                                      ),
+                                      const SizedBox(height: 10),
+                                    ],
+                                  ),
+                                )
+                                .toList(),
+                          ),
+                        ),
+                      )
+                    : SizedBox(
+                        height: MediaQuery.of(context).size.height * 0.5,
+                        child: Center(
+                          child: Text(
+                            'No data found',
+                            style: TextStyle(
+                              color: theme.darkTheme
+                                  ? Theme.of(context).primaryColor
+                                  : Colors.black,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 25,
+                            ),
+                          ),
+                        ),
+                      ),
               ],
             ),
           ),
